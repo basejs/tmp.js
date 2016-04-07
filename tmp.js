41b13293423ff10564c805e15b7bbab9fe84ebf8
basejs <admin@naai.net>
2016-04-07 23:01:24 +0800

    新增else if

--- tmp.js
+++ tmp.js
@@ -1,9 +1,9 @@
 /*!
- * tmp.js v0.1.0
+ * tmp.js v0.1.1
  * github https://github.com/basejs/tmp
  *
  * Copyright zhuqiang
- * Date: 2016/03/21
+ * Date: 2016/04/07
  */
 ;(function(exports) {
 	var tmp = function(tmphtml,data) {
@@ -26,19 +26,27 @@
 
 				case /^{@\/if[^{}]*}$/.test(item):
 					return '}';
+
 				case /^{@\/each[^{}]*}$/.test(item):
 					return '}})();';
-				case /^{@else[^{}]*}$/.test(item):
+
+				case /^{@else\s*}$/.test(item):
 					return '}else{';
+
 				case /^{@each[^{}]*}$/.test(item):
 					//each返回for循环，尽量不要用t做索引
 					return item.replace(/{@each\s+(\w+)\s+as\s+(\w+)\s*(?:,\s*(\w+)\s*)?}/,function($1,$2,$3,$4){
 						//console.log($1+'-----'+$2+'-----'+$3+'-----'+$4);
-						return '(function(){for(var t=0; t<'+ $2 +'.length; t++){'+ $3 +'=' + $2 +'[t];' + $4 +'=t;';
+						return '(function(){var xps=0, '+ $4 +';for(; xps<'+ $2 +'.length; xps++){'+ $3 +'=' + $2 +'[xps];'+ $4 +'=xps;';
 					});
 				case /^{@if[^{}]*}$/.test(item):
 					//if直接返回条件
-					return item.replace(/{@if\s+?([^{}]+)\}/,'if($1){');
+					return item.replace(/{@if\s*\(?([^{}()]+)\)?\s*}/,'if($1){');
+
+				case /^{@else\s+if/.test(item):
+					//else if
+					return item.replace(/{@else\s+if\s*\(?([^{}()]+)\)?\s*}/,'}else if($1){');
+
 				case /^{[^{}]+}$/.test(item):
 					//变量返回当前作用域或全局作用域
 					item = item.replace(/{\s*([^{}]+)\s*}/g,'$1');
@@ -52,6 +60,7 @@
 			}
 		}).join('');
 		newstr += '})();return tmpstr;';
+		//console.log(newstr);
 		return new Function(newstr)();
 	};
 
